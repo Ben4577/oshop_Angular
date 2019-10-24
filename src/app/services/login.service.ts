d9c23bcf92a28551b177669e7a0b71002c076a52
Ben Kellington <Ben.Kellington@minsterlaw.co.uk>
2019-10-24 16:27:30 +0100

    wewe

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { getDefaultService } from 'selenium-webdriver/opera';
+import { User } from '../models/user'
+import { Observable, empty, of } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+user: User;
+
+constructor() {  
+
+}
+
+
+public getUser(): Observable<User> 
+{
+  return of (this.user);
+}
+
+
+public setUser()
+{
+  this.user = {
+    displayName: "ben kellington",
+    password: "password"
+}
+
+console.log(this.user);
+}
+
+
+
+}
