72298f37115f98b899280e23457f86c68de54639
Ben Kellington <Ben.Kellington@minsterlaw.co.uk>
2019-11-24 15:16:41 +0000

    new update

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../../models/Products'
 import { Subscription } from 'rxjs';
+declare var $;
+
 
 @Component({
   selector: 'app-admin-products',
@@ -10,29 +12,57 @@ import { Subscription } from 'rxjs';
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
 
+  dataTable: any;
+
+
 products: Product[];
 filteredProducts: Product[];
 subscription: Subscription;
+//tableRsource: DataTableResource<Product>;
+items: Product[] = [];
+itemCount: Number;
+
+
+
 
   constructor(private productService: ProductService) { 
     this.subscription = this.productService.getAll().subscribe(result =>
       {        
-     this.filteredProducts = this.products = result;
+      this.filteredProducts = this.products = result;
+     // this.intializeTable(result);
       });
     }
 
-filter(query: string) {
-console.log(query);
-this.filteredProducts = (query) ? this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
-}
 
-  ngOnInit() {
-  }
+ // private intializeTable(products: Product[]){
+   // this.tableRsource = new DataTableResource(this.products);
+  //  this.tableRsource.query({ offset: 0})
+  //  .then(items => this.items = items);
+   // this.tableRsource.count()
+   // .then(count => this.itemCount = count);
+   // }
+
+
+   // reloadItems(params) {
+   //   if(!this.tableRsource) return;
+   //   this.tableRsource.query(params)
+    //  .then(items => this.items = items);
+   // }
+
+  //filter(query: string) {
+  //console.log(query);
+  //this.filteredProducts = (query) ? this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
+ //}
 
 
   ngOnDestroy() {
   this.subscription.unsubscribe();
   }
 
+
+  ngOnInit() {
+    //this.dataTable = $(this.table.nativeElement);
+    //this.dataTable.data;
+  }
 
 }
--- src/app/models/Products.ts
+++ src/app/models/Products.ts
@@ -1,4 +1,4 @@
-export class Product {
+export interface Product {
     id: string;
     title: string;
     price: number;
